589ce3519b588f4fcec20d02d72f8ef09981a81a
DevTamakuwala <dev.official2424@gmail.com>
2025-07-21 15:31:24 +0530

    initial commit.

---
+++ src/test/java/io/github/devtamakuwala/dailydine/DailyDineApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.devtamakuwala.dailydine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyDineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
